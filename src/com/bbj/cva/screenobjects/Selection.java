f7bd4c8b4fee5f48464a3ec9384e4f8a096f5e8a
Brad Baillio <brad.baillio@gmail.com>
2013-01-27 17:43:32 -0700

    adjustments to not go off the screen w Selection

--- src/com/bbj/cva/screenobjects/Selection.java
+++ src/com/bbj/cva/screenobjects/Selection.java
@@ -33,16 +33,35 @@ public class Selection implements ScreenObjectBase {
 		 * Detect requested motion.
 		 */
 
-		if (now - lastMoveSelectionTime > SELECTION_WAIT) {
-			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
-				selectionRect.x += CvaModel.TILE_WIDTH;
-			} else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
-				selectionRect.x -= CvaModel.TILE_WIDTH;
+		if (now - lastMoveSelectionTime > SELECTION_WAIT) 
+		{
+			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) 
+			{
+				if (selectionRect.x + CvaModel.TILE_WIDTH < CvaModel.screenWidth)
+					selectionRect.x += CvaModel.TILE_WIDTH;
+			} 
+			else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) 
+			{
+				if (selectionRect.x - CvaModel.TILE_WIDTH >= 0)
+					selectionRect.x -= CvaModel.TILE_WIDTH;
 			}
-			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
-				selectionRect.y += CvaModel.TILE_HEIGHT;
-			} else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
-				selectionRect.y -= CvaModel.TILE_HEIGHT;
+			if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) 
+			{
+				// FIXME: This has an issue because the tile height is not a multiple of the ScreenHeight
+				if (selectionRect.y + CvaModel.TILE_HEIGHT < CvaModel.screenHeight)
+					selectionRect.y += CvaModel.TILE_HEIGHT;
+			}
+			else if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) 
+			{
+				if (selectionRect.y - CvaModel.TILE_HEIGHT >= 0)
+					selectionRect.y -= CvaModel.TILE_HEIGHT;
+			}
+			if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
+			{
+				// TODO: This should call a Factory to create a new object
+				//        ** Instead of "creating" a new object...should it just grab an already created one?
+				//        ** What are our memory limitations?  Standard on dynamic memory allocation?
+				//       Then it needs to get added to the ScreenObject Array for the CVA::render() call.
 			}
 			lastMoveSelectionTime = now;
 		}
